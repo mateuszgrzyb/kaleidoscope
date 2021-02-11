2d3b9b43c60830b93811aa72da24a054b16e1238
Mateusz Grzyb <mateusz.j.grzyb@protonmail.com>
2021-02-11 01:06:16 +0100

    init

---
+++ src/Codegen.hs
@@ -0,0 +1,13 @@
+
+module Codegen where
+
+-- double :: Type
+-- double = FloatingPointType 64 IEEE
+-- 
+-- type SymbolTable = [(String, Operand)]
+-- 
+-- data CodegenState = CodegenState
+--     { 
+-- 
+--     }
+
